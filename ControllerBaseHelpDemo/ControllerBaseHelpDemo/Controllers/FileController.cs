284d3d1f38a92cc609f7eefed1fd2fd1cfb4b1e2
kkbruce <bruce.mvc@outlook.com>
2020-02-06 22:06:52 +0800

    update comment

--- ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
+++ ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
@@ -7,7 +7,8 @@ namespace ControllerBaseHelpDemo.Controllers
     public class FileController : ControllerBase
     {
         /// <summary>
-        /// It will look for the "wwwroot" directory
+        /// (string, string) It will look for the "wwwroot" directory
+        /// (string, string) 會到 wwwroot 目錄尋找檔案
         /// </summary>
         /// <returns>VirtualFileResult</returns>
         public IActionResult DemoFile1()
@@ -15,6 +16,11 @@ namespace ControllerBaseHelpDemo.Controllers
             return File("files\\TaiwanNo1.txt", "application/octet-stream");
         }
 
+        /// <summary>
+        /// (string, string) It will look for the "wwwroot" directory and set download name.
+        /// (string, string) 會到 wwwroot 目錄尋找檔案，並設定下載檔案名稱
+        /// </summary>
+        /// <returns>VirtualFileResult</returns>
         public IActionResult DemoFile2()
         {
             return File("files\\TaiwanNo1.txt", "application/octet-stream", "SkilltreeNo1.txt");
