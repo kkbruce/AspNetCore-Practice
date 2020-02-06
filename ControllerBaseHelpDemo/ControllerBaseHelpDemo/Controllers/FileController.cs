489731dfbe713ccaee3b06dc74c61aabce590293
kkbruce <bruce.mvc@outlook.com>
2020-02-06 22:01:35 +0800

    remove non-use namespace.

--- ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
+++ ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ControllerBaseHelpDemo.Controllers
 {
