987e64e41ee574f385c939a389f9c555b80a02be
kkbruce <bruce.mvc@outlook.com>
2020-02-06 21:59:52 +0800

    Add partial File Helper

---
+++ ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ControllerBaseHelpDemo.Controllers
+{
+    [Route("File/[action]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        /// <summary>
+        /// It will look for the "wwwroot" directory
+        /// </summary>
+        /// <returns>VirtualFileResult</returns>
+        public IActionResult DemoFile1()
+        {
+            return File("files\\TaiwanNo1.txt", "application/octet-stream");
+        }
+
+        public IActionResult DemoFile2()
+        {
+            return File("files\\TaiwanNo1.txt", "application/octet-stream", "SkilltreeNo1.txt");
+        }
+    }
+}
