09917734991d854629cc1cf23d56d01f88ebd3ff
Bruce Chen <brucechen@kingstono365.onmicrosoft.com>
2020-02-07 14:23:24 +0800

    Add File(byte[], string) demo code.

--- ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
+++ ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControllerBaseHelpDemo.Controllers
 {
@@ -6,12 +7,31 @@ namespace ControllerBaseHelpDemo.Controllers
     [ApiController]
     public class FileController : ControllerBase
     {
+        private readonly IWebHostEnvironment _environment;
+
+        public FileController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        /// <summary>
+        /// (byte[], string) return byte[] data.
+        /// </summary>
+        /// <returns>FileContentResult</returns>
+        public IActionResult DemoFile1()
+        {
+            // Need injection IWebHostEnvironment to wwwroot path.
+            var wwwroot = _environment.WebRootPath;
+            var sameple = System.IO.File.ReadAllBytes($"{wwwroot}/files/Sample.pdf");
+            return File(sameple, "application/pdf");
+        }
+
         /// <summary>
         /// (string, string) It will look for the "wwwroot" directory
         /// (string, string) 會到 wwwroot 目錄尋找檔案
         /// </summary>
         /// <returns>VirtualFileResult</returns>
-        public IActionResult DemoFile1()
+        public IActionResult DemoFile2()
         {
             return File("files\\TaiwanNo1.txt", "application/octet-stream");
         }
@@ -21,7 +41,7 @@ namespace ControllerBaseHelpDemo.Controllers
         /// (string, string) 會到 wwwroot 目錄尋找檔案，並設定下載檔案名稱
         /// </summary>
         /// <returns>VirtualFileResult</returns>
-        public IActionResult DemoFile2()
+        public IActionResult DemoFile3()
         {
             return File("files\\TaiwanNo1.txt", "application/octet-stream", "SkilltreeNo1.txt");
         }
