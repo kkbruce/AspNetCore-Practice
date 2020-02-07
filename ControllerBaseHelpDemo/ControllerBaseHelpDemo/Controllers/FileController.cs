2fa1bc634718fc19c9ec60f7114b2dd3490e35d2
Bruce Chen <brucechen@kingstono365.onmicrosoft.com>
2020-02-07 15:35:11 +0800

    Add File with FileStream partial sample

--- ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
+++ ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/FileController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
@@ -27,7 +28,7 @@ namespace ControllerBaseHelpDemo.Controllers
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile1()
         {
-            var fileContents = GetfileContents();
+            var fileContents = GetFileByte();
             return File(fileContents, "application/pdf");
         }
 
@@ -38,7 +39,7 @@ namespace ControllerBaseHelpDemo.Controllers
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile2()
         {
-            var fileContents = GetfileContents();
+            var fileContents = GetFileByte();
             return File(fileContents, "application/pdf", true);
         }
 
@@ -49,7 +50,7 @@ namespace ControllerBaseHelpDemo.Controllers
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile3()
         {
-            var fileContents = GetfileContents();
+            var fileContents = GetFileByte();
             return File(fileContents, "application/pdf", "ASPNetCoreNo1.pdf");
         }
 
@@ -60,7 +61,7 @@ namespace ControllerBaseHelpDemo.Controllers
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile4()
         {
-            var fileContents = GetfileContents();
+            var fileContents = GetFileByte();
             return File(fileContents, "application/pdf", "ASPNetCoreNo1.pdf", true);
         }
 
@@ -71,7 +72,7 @@ namespace ControllerBaseHelpDemo.Controllers
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile5()
         {
-            var fileContents = GetFileContentsWithEtag(out var wwwroot, out var lastModified, out var entityTag);
+            var fileContents = GetFileByteWithEtag(out var wwwroot, out var lastModified, out var entityTag);
             return File(fileContents, "application/pdf", lastModified, entityTag);
         }
 
@@ -82,7 +83,7 @@ namespace ControllerBaseHelpDemo.Controllers
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile6()
         {
-            var fileContents = GetFileContentsWithEtag(out var wwwroot, out var lastModified, out var entityTag);
+            var fileContents = GetFileByteWithEtag(out var wwwroot, out var lastModified, out var entityTag);
             return File(fileContents, "application/pdf", lastModified, entityTag, true);
         }
 
@@ -93,35 +94,88 @@ namespace ControllerBaseHelpDemo.Controllers
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile7()
         {
-            var fileContents = GetFileContentsWithEtag(out var wwwroot, out var lastModified, out var entityTag);
+            var fileContents = GetFileByteWithEtag(out var wwwroot, out var lastModified, out var entityTag);
             return File(fileContents, "application/pdf", "ASPNetCoreNo1.pdf", lastModified, entityTag);
         }
 
         /// <summary>
         /// Return byte[] data and set "Last-Modified" and "ETag" header information and set download name and enable range processing.
-        /// 回傳 byte[] 資料，並設定 "Last-Modified" 和 "ETag" 標頭資訊，並設定下載檔案名稱，與啟用部份請求的處理(partial requests)。s
+        /// 回傳 byte[] 資料，並設定 "Last-Modified" 和 "ETag" 標頭資訊，並設定下載檔案名稱，與啟用部份請求的處理(partial requests)。
         /// </summary>
         /// <returns>FileContentResult</returns>
         public IActionResult DemoFile8()
         {
-            var fileContents = GetFileContentsWithEtag(out var wwwroot, out var lastModified, out var entityTag);
+            var fileContents = GetFileByteWithEtag(out var wwwroot, out var lastModified, out var entityTag);
             return File(fileContents, "application/pdf", "ASPNetCoreNo1.pdf", lastModified, entityTag, true);
         }
 
-        private byte[] GetfileContents()
+        private byte[] GetFileByte()
         {
             var wwwroot = _environment.WebRootPath;
             var fileContents = System.IO.File.ReadAllBytes($"{wwwroot}/files/Sample.pdf");
             return fileContents;
         }
 
-        private byte[] GetFileContentsWithEtag(out string wwwroot, out DateTimeOffset lastModified, out EntityTagHeaderValue entityTag)
+        private byte[] GetFileByteWithEtag(out string wwwroot, out DateTimeOffset lastModified, out EntityTagHeaderValue entityTag)
         {
             wwwroot = _environment.WebRootPath;
             var fileContents = System.IO.File.ReadAllBytes($"{wwwroot}/files/Sample.pdf");
             lastModified = DateTimeOffset.Parse("2020/02/07 14:21:13 PM");
             entityTag = new EntityTagHeaderValue("\"Etag\"");
             return fileContents;
+        }
+
+        /// <summary>
+        /// Return Stream data.
+        /// 回傳 Stream 型別資料。
+        /// </summary>
+        /// <returns>FileStreamResult</returns>
+        public IActionResult DemoFile9()
+        {
+            var fileStream = GetFileStream();
+
+            return File(fileStream, "application/pdf");
+        }
+
+        /// <summary>
+        /// Return Stream data and enable range processing.
+        /// 回傳 Stream 型別資料，與啟用部份請求的處理(partial requests)。
+        /// </summary>
+        /// <returns>FileStreamResult</returns>
+        public IActionResult DemoFile10()
+        {
+            var fileStream = GetFileStream();
+
+            return File(fileStream, "application/pdf", true);
+        }
+
+        /// <summary>
+        /// Return Stream data and set download file.
+        /// 回傳 Stream 型別資料，並設定下載檔案名稱。
+        /// </summary>
+        /// <returns>FileStreamResult</returns>
+        public IActionResult DemoFile11()
+        {
+            var fileStream = GetFileStream();
+            return File(fileStream, "application/pdf", "ASPNetCoreNo1.pdf");
+        }
+
+        /// <summary>
+        /// Return Stream data and set download file and enable range processing.
+        /// 回傳 Stream 型別資料，並設定下載檔案名稱，與啟用部份請求的處理(partial requests)。
+        /// </summary>
+        /// <returns>FileStreamResult</returns>
+        public IActionResult DemoFile12()
+        {
+            var fileStream = GetFileStream();
+            return File(fileStream, "application/pdf", "ASPNetCoreNo1.pdf", true);
+        }
+
+        private FileStream GetFileStream()
+        {
+            var wwwroot = _environment.WebRootPath;
+            var fileStream = new FileStream($"{wwwroot}/files/Sample.pdf", FileMode.Open);
+            return fileStream;
         }
 
         /// <summary>
