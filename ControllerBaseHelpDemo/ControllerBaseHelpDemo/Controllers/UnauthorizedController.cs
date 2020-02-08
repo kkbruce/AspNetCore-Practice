9f72749a11c63d797411917536a675c7c0344b84
kkbruce <bruce.mvc@outlook.com>
2020-02-08 14:12:24 +0800

    Add Unauthorized sample code

---
+++ ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/UnauthorizedController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerBaseHelpDemo.Controllers
+{
+    [Route("Unauthorized/[action]")]
+    [ApiController]
+    public class UnauthorizedController : ControllerBase
+    {
+        /// <summary>
+        /// Return HTTP 401
+        /// 回傳 HTTP 401
+        /// </summary>
+        /// <returns>UnauthorizedResult</returns>
+        public IActionResult DemoUnauthorized1()
+        {
+            return Unauthorized();
+        }
+
+        /// <summary>
+        /// Return HTTP 401 and custom message object.
+        /// 回傳 HTTP 401 與自訂訊息物件。
+        /// </summary>
+        /// <returns>UnauthorizedObjectResult</returns>
+        public IActionResult DemoUnauthorized2()
+        {
+            var obj = new
+            {
+                Error = "You are unauthorized."
+            };
+            return Unauthorized(obj);
+        }
+    }
+}
