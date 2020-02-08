1a11b3863153f60bf498dc19fea3c143dbeca975
kkbruce <bruce.mvc@outlook.com>
2020-02-08 21:49:10 +0800

    Add BadRequest sample

---
+++ src/IActionResultSample/Controllers/BadRequestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IActionResultSample.Controllers
+{
+    [Route("BadRequest/[action]")]
+    [ApiController]
+    public class BadRequestController : ControllerBase
+    {
+        /// <summary>
+        /// Return HTTP 400
+        /// 回傳 HTTP 400
+        /// </summary>
+        /// <returns>BadRequestResult</returns>
+        public IActionResult Demo1()
+        {
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// Return HTTP 400 and custom message object.
+        /// </summary>
+        /// <returns>BadRequestObjectResult</returns>
+        public IActionResult Demo2()
+        {
+            var obj = new
+            {
+                Error = "Bad Request."
+            };
+            return BadRequest(obj);
+        }
+
+        /// <summary>
+        /// Return HTTP 400 and ModelState( with MVC)
+        /// </summary>
+        /// <returns>BadRequestObjectResult</returns>
+        public IActionResult Demo3()
+        {
+            var msd = new ModelStateDictionary();
+            return BadRequest(msd);
+        }
+    }
+}
