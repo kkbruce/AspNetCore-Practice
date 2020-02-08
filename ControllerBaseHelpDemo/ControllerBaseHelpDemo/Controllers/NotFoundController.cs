ce12dc76d8a4a11e326f2962be4a43623587d2ae
kkbruce <bruce.mvc@outlook.com>
2020-02-08 16:02:03 +0800

    Add NotFound sample code

---
+++ ControllerBaseHelpDemo/ControllerBaseHelpDemo/Controllers/NotFoundController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerBaseHelpDemo.Controllers
+{
+    [Route("NotFound/[action]")]
+    [ApiController]
+    public class NotFoundController : ControllerBase
+    {
+        /// <summary>
+        /// Return HTTP 404
+        /// 回傳 HTTP 404
+        /// </summary>
+        /// <returns>NotFoundResult</returns>
+        public IActionResult DemoNotFound1()
+        {
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Return HTTP 404 and custom message object.
+        /// 回傳 HTTP 404 與自訂訊息物件。
+        /// </summary>
+        /// <returns>NotFoundObjectResult</returns>
+        public IActionResult DemoNotFound2()
+        {
+            var obj = new
+            {
+                Error = "You are unauthorized."
+            };
+            return NotFound(obj);
+        }
+    }
+}
