fbb56bc8fdb9ad8509d2b248b78c2c877b13e0a9
kkbruce <bruce.mvc@outlook.com>
2020-02-09 22:20:04 +0800

    Add CreatedAtAction sample

--- src/IActionResultSample/Controllers/CreatedController.cs
+++ src/IActionResultSample/Controllers/CreatedController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IActionResultSample.Controllers
@@ -43,5 +44,40 @@ namespace IActionResultSample.Controllers
             };
             return Created(uri, obj);
         }
-    }
+
+        /// Return HTTP 201 and include Location header value.
+        /// 回傳 HTTP 201 and Location 標頭的 URI 值。
+        /// </summary>
+        /// <returns>CreatedAtActionResult</returns>
+        public IActionResult Demo4()
+        {
+            return CreatedAtAction("DemoAction", new { Id = 1 }, null);
+        }
+
+        /// <summary>
+        /// Return HTTP 201 and include Location header value and object value to format in the entity body
+        /// 回傳 HTTP 201 and Location 標頭自訂物件。
+        /// </summary>
+        /// <returns>CreatedAtActionResult</returns>
+        public IActionResult Demo5()
+        {
+            var obj = new
+            {
+                Name = "Skilltree",
+                Event = "ASP.NET Core Web API",
+                URL = new Uri("https://skilltree.my")
+            };
+            return CreatedAtAction("DemoAction", new { Id = 1 }, obj);
+        }
+
+        /// <summary>
+        /// Get Location header RUI data.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns>ContentResult</returns>
+        public IActionResult DemoAction(int Id)
+        {
+            return Content($"取得Id={Id}", "text/plain", Encoding.Unicode);
+        }
+}
 }
