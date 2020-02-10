c1c13a505da785e6ebaf353e7d02da21b464fa26
Bruce Chen <brucechen@kingstono365.onmicrosoft.com>
2020-02-10 11:10:50 +0800

    Add Accepted sample

---
+++ src/IActionResultSample/Controllers/AcceptedController.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+
+namespace IActionResultSample.Controllers
+{
+    [Route("Accepted/[action]")]
+    [ApiController]
+    public class AcceptedController : ControllerBase
+    {
+        /// <summary>
+        /// Return HTTP 202
+        /// 回傳 HTTP 202
+        /// </summary>
+        /// <returns>AcceptedResult</returns>
+        public IActionResult Demo1()
+        {
+            return Accepted();
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and custom message object.
+        /// 回傳 HTTP 202 和自訂訊息物件。
+        /// </summary>
+        /// <returns>AcceptedResult</returns>
+        public IActionResult Demo2()
+        {
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return Accepted(obj);
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value.
+        /// 回傳 HTTP 202 和 Location 標頭與值。
+        /// </summary>
+        /// <returns>AcceptedResult</returns>
+        public IActionResult Demo3()
+        {
+            var uri = new Uri("https://skilltree.my");
+            return Accepted(uri);
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value.
+        /// 回傳 HTTP 202 和 Location 標頭與值。
+        /// </summary>
+        /// <returns>AcceptedResult</returns>
+        public IActionResult Demo4()
+        {
+            return Accepted("https://skilltree.my");
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value and custom message object.
+        /// 回傳 HTTP 202 和 Location 標頭與值和自訂訊息物件。
+        /// </summary>
+        /// <returns>AcceptedResult</returns>
+        public IActionResult Demo5()
+        {
+            var uri = new Uri("https://skilltree.my");
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return Accepted(uri, obj);
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value and custom message object.
+        /// 回傳 HTTP 202 和 Location 標頭與值和自訂訊息物件。
+        /// </summary>
+        /// <returns>AcceptedResult</returns>
+        public IActionResult Demo6()
+        {
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return Accepted("https://skilltree.my", obj);
+        }
+
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value, URI use ActionName to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值，URI 使用 ActionName 來產生。
+        /// </summary>
+        /// <returns>AcceptedAtActionResult</returns>
+        public IActionResult Demo7()
+        {
+            return AcceptedAtAction("DemoAction");
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value, URI use ActionName and ControllerName to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值，URI 使用 ActionName 和 ControllerName 來產生。
+        /// </summary>
+        /// <returns>AcceptedAtActionResult</returns>
+        public IActionResult Demo8()
+        {
+            return AcceptedAtAction("DemoUnicode", "Content");
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value and custom message object, URI use ActionName to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值和自訂訊息物件，URI 使用 ActionName 來產生。
+        /// </summary>
+        /// <returns>AcceptedAtActionResult</returns>
+        public IActionResult Demo9()
+        {
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return AcceptedAtAction("DemoAction", obj);
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value, URI use ActionName and ControllerName to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值，URI 使用 ActionName 和 ControllerName 來產生。
+        /// </summary>
+        /// <returns>AcceptedAtActionResult</returns>
+        public IActionResult Demo10()
+        {
+            return AcceptedAtAction("DemoAction", "Accepted", new { id = 1 });
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value and custom message object, URI use ActionName to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值和自訂訊息物件，URI 使用 ActionName 來產生。
+        /// </summary>
+        /// <returns>AcceptedAtActionResult</returns>
+        public IActionResult Demo11()
+        {
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return AcceptedAtAction("DemoAction", new { id = 1 }, obj);
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value, URI use ActionName and ControllerName to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值，URI 使用 ActionName 和 ControllerName 來產生。
+        /// </summary>
+        /// <returns>AcceptedAtActionResult</returns>
+        public IActionResult Demo12()
+        {
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return AcceptedAtAction("DemoAction", "Accepted", new { id = 1 }, obj);
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value, URI use default Route and RouteValue to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值，URI 使用預設 Route 和 RouteValue 產生。
+        /// </summary>
+        /// <returns>AcceptedAtRouteResult</returns>
+        public IActionResult Demo13()
+        {
+            return AcceptedAtRoute(new { id = 1 });
+        }
+
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value, URI use RouteName to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值，URI 使用 RouteName 產生。
+        /// </summary>
+        /// <returns>AcceptedAtRouteResult</returns>
+        public IActionResult Demo14()
+        {
+            return AcceptedAtRoute("AcceptedRoute");
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value, URI use RouteName and RouteValue to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值，URI 使用 RouteName 和 RouteValue 產生。
+        /// </summary>
+        /// <returns>AcceptedAtRouteResult</returns>
+        public IActionResult Demo15()
+        {
+            return AcceptedAtRoute("AcceptedRoute", new { id = 1 });
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value and custom message object, URI use RouteValue to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值和自訂訊息物件，URI 使用 RouteValue 產生。
+        /// </summary>
+        /// <returns>AcceptedAtRouteResult</returns>
+        public IActionResult Demo16()
+        {
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return AcceptedAtRoute(new { id = 1 }, obj);
+        }
+
+        /// <summary>
+        /// Return HTTP 202 and Location header value and custom message object, URI use RouteName and RouteValue to generate.
+        /// 回傳 HTTP 202 和 Location 標頭與值和自訂訊息物件，URI 使用 RouteName 和 RouteValue 產生。
+        /// </summary>
+        /// <returns>AcceptedAtRouteResult</returns>
+        public IActionResult Demo17()
+        {
+            var obj = new
+            {
+                Message = "Accepted."
+            };
+            return AcceptedAtRoute("AcceptedRoute", new { id = 1 }, obj);
+        }
+
+        /// <summary>
+        /// For test generate new URI.
+        /// 可測試新產生的 URI。
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns>ContentResult</returns>
+        public IActionResult DemoAction(int? id)
+        {
+            return Content($"取得Id={id}", "text/plain", Encoding.Unicode);
+        }
+
+        /// <summary>
+        /// For test generate new URI.
+        /// 可測試新產生的 URI。
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns>ContentResult</returns>
+        [Route("/Accepted/DemoRoute", Name = "AcceptedRoute")]
+        public IActionResult DemoRoute(int? id)
+        {
+            return Content($"取得Id={id}", "text/plain", Encoding.Unicode);
+        }
+    }
+}
--- src/IActionResultSample/Controllers/CreatedController.cs
+++ src/IActionResultSample/Controllers/CreatedController.cs
@@ -46,8 +46,8 @@ namespace IActionResultSample.Controllers
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value.
-        /// 回傳 HTTP 201 and Location 標頭與值。
+        /// Return HTTP 201 and Location header value, URI use ActionName to generate.
+        /// 回傳 HTTP 201 and Location 標頭與值，URI 使用 ActionName 來產生。
         /// </summary>
         /// <returns>CreatedAtActionResult</returns>
         public IActionResult Demo4()
@@ -56,8 +56,8 @@ namespace IActionResultSample.Controllers
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value and custom message object.
-        /// 回傳 HTTP 201 and Location 標頭與值和自訂訊息物件。
+        /// Return HTTP 201 and Location header value, URI use ActionName to generate.
+        /// 回傳 HTTP 201 and Location 標頭與值和自訂訊息物件，URI 使用 ActionName 來產生。
         /// </summary>
         /// <returns>CreatedAtActionResult</returns>
         public IActionResult Demo5()
@@ -72,19 +72,18 @@ namespace IActionResultSample.Controllers
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value and custom message object.
-        /// 回傳 HTTP 201 and Location 標頭與值和自訂訊息物件。。
+        /// Return HTTP 201 and Location header value, URI use ActionName and ControllerName to generate.
+        /// 回傳 HTTP 201 and Location 標頭與值，URI 使用 ActionName 和 ControllerName 來產生。
         /// </summary>
         /// <returns>CreatedAtActionResult</returns>
         public IActionResult Demo6()
         {
-            // Create other controller/action RUI for Location header.
             return CreatedAtAction("DemoUnicode", "Content", new { id = 1 }, null);
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value and custom message object.
-        /// 回傳 HTTP 201 and Location 標頭與自訂訊息物件。
+        /// Return HTTP 201 and Location header value and custom message object, URI use ActionName and ControllerName to generate.
+        /// 回傳 HTTP 201 and Location 標頭與自訂訊息物件，URI 使用 ActionName 和 ControllerName 來產生。
         /// </summary>
         /// <returns>CreatedAtActionResult</returns>
         public IActionResult Demo7()
@@ -93,23 +92,22 @@ namespace IActionResultSample.Controllers
             {
                 Message = "Created."
             };
-            // Create other controller/action RUI for Location header.
             return CreatedAtAction("DemoUnicode", "Content", new { id = 1 }, obj);
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value.
-        /// 回傳 HTTP 201 and Location 標頭與值。
+        /// Return HTTP 201 and Location header value, URI use RouteName to generate.
+        /// 回傳 HTTP 201 and Location 標頭與值，URI 使用 RouteName 來產生。
         /// </summary>
         /// <returns>CreatedAtRouteResult</returns>
         public IActionResult Demo8()
         {
-            return CreatedAtRoute("SampleRoute", null);
+            return CreatedAtRoute("CreatedRoute", null);
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value.
-        /// 回傳 HTTP 201 and Location 標頭與值。
+        /// Return HTTP 201 and Location header value and custom message object, URI use RouteName to generate.
+        /// 回傳 HTTP 201 and Location 標頭與值與自訂訊息物件，URI 使用 RouteName 來產生。
         /// </summary>
         /// <returns>CreatedAtRouteResult</returns>
         public IActionResult Demo9()
@@ -118,12 +116,12 @@ namespace IActionResultSample.Controllers
             {
                 Message = "Created."
             };
-            return CreatedAtRoute("SampleRoute", obj);
+            return CreatedAtRoute("CreatedRoute", obj);
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value.
-        /// 回傳 HTTP 201 and Location 標頭與值。
+        /// Return HTTP 201 and Location header value and custom message object, URI use default Route to generate.
+        /// 回傳 HTTP 201 and Location 標頭與值與自訂訊息物件，URI 使用預設 Route 來產生。
         /// </summary>
         /// <returns>CreatedAtRouteResult</returns>
         public IActionResult Demo10()
@@ -136,8 +134,8 @@ namespace IActionResultSample.Controllers
         }
 
         /// <summary>
-        /// Return HTTP 201 and include Location header value.
-        /// 回傳 HTTP 201 and Location 標頭與值。
+        /// Return HTTP 201 and Location header value and custom message object, URI use RouteName to generate.
+        /// 回傳 HTTP 201 and Location 標頭與值與自訂訊息物件，URI 使用 RouteName 來產生。
         /// </summary>
         /// <returns>CreatedAtRouteResult</returns>
         public IActionResult Demo11()
@@ -146,7 +144,7 @@ namespace IActionResultSample.Controllers
             {
                 Message = "Created."
             };
-            return CreatedAtRoute("SampleRoute", new { id = 1 }, obj);
+            return CreatedAtRoute("CreatedRoute", new { id = 1 }, obj);
         }
 
         /// <summary>
@@ -166,7 +164,7 @@ namespace IActionResultSample.Controllers
         /// </summary>
         /// <param name="id">Id</param>
         /// <returns>ContentResult</returns>
-        [Route("/DemoRoute", Name = "SampleRoute")]
+        [Route("/Created/DemoRoute", Name = "CreatedRoute")]
         public IActionResult DemoRoute(int? id)
         {
             return Content($"取得Id={id}", "text/plain", Encoding.Unicode);
