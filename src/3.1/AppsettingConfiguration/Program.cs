53dd49018e35dcb296e5aeb771310a24e08ba4c5
kkbruce <bruce.mvc@outlook.com>
2020-02-16 13:45:41 +0800

    Add Print method.

--- src/3.1/AppsettingConfiguration/Program.cs
+++ src/3.1/AppsettingConfiguration/Program.cs
@@ -9,15 +9,22 @@ namespace AppsettingConfiguration
         static void Main(string[] args)
         {
             var config = Startup.Configuration();
-            Demo1(config);
+            GetKeyValue(config);
+            Console.Read();
         }
 
-        public static void Demo1(IConfigurationRoot config)
+        static void GetKeyValue(IConfigurationRoot config)
         {
             var server = config.GetSection("Server").Value;
             var user = config.GetSection("User").Value;
             var pwd = config.GetSection("Pwd").Value;
-            Console.WriteLine($"Server: {server}, User: {user}, Pwd: {pwd}");
+            Print($"Server: {server}, User: {user}, Pwd: {pwd}");
+        }
+
+        static void Print(string configString)
+        {
+            Console.WriteLine(configString);
+            Console.WriteLine("--------------------");
         }
     }
 }
