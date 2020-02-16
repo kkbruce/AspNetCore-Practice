7bc51f100d8b326c7ea75855e92045aed17fe2a6
kkbruce <bruce.mvc@outlook.com>
2020-02-16 13:52:35 +0800

    Add GetHierarchicalData Sample

--- src/3.1/AppsettingConfiguration/Program.cs
+++ src/3.1/AppsettingConfiguration/Program.cs
@@ -10,10 +10,26 @@ namespace AppsettingConfiguration
         {
             var config = Startup.Configuration();
             GetKeyValue(config);
+            GetConnectionString(config);
+            GetHierarchicalData(config);
+
             Console.Read();
         }
 
-        static void GetKeyValue(IConfigurationRoot config)
+        private static void GetHierarchicalData(IConfigurationRoot config)
+        {
+            var appName = config.GetSection("AppInfo.AppName").Value;
+            var maintainer = config.GetSection("AppInfo:Maintainer").Value;
+            Print($"AppName: {appName}, Maintainer: {maintainer}");
+        }
+
+        private static void GetConnectionString(IConfigurationRoot config)
+        {
+            var conn = config.GetConnectionString("DefaultConnection");
+            Print($"ConnectionString: {conn}");
+        }
+
+        private static void GetKeyValue(IConfigurationRoot config)
         {
             var server = config.GetSection("Server").Value;
             var user = config.GetSection("User").Value;
@@ -21,10 +37,10 @@ namespace AppsettingConfiguration
             Print($"Server: {server}, User: {user}, Pwd: {pwd}");
         }
 
-        static void Print(string configString)
+        private static void Print(string configString)
         {
             Console.WriteLine(configString);
-            Console.WriteLine("--------------------");
+            Console.WriteLine("------------------------------------------------------------");
         }
     }
 }
