3fdedc1664c1f93097f6646a286f5e34831403cd
kkbruce <bruce.mvc@outlook.com>
2020-02-17 22:24:06 +0800

    Add MultiFilesSaveDisk sample

--- src/3.1/FileUploadSample/Controllers/UploadController.cs
+++ src/3.1/FileUploadSample/Controllers/UploadController.cs
@@ -55,7 +55,7 @@ namespace FileUploadSample.Controllers
         }
 
         /// <summary>
-        /// Upload a file save to Upload folder.
+        /// Upload a file and save to Upload folder.
         /// 上傳一個檔案並儲存至Upload資料夾下。
         /// </summary>
         /// <param name="file">upload key name.</param>
@@ -133,6 +133,32 @@ namespace FileUploadSample.Controllers
                 }
             }
             return Ok(fileInfo);
+        }
+
+        /// <summary>
+        /// Upload one or multi files and save to Upload folder.
+        /// 上傳一到多個檔案並儲存至Upload資料夾下。
+        /// </summary>
+        /// <param name="files">upload key name.</param>
+        /// <returns>HTTP 200</returns>
+        public async Task<IActionResult> MultiFilesSaveDisk([FromForm] List<IFormFile> files)
+        {
+            string fileInfo = string.Empty;
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    var filePath = Path.Combine(_hostingEnvironment.ContentRootPath,
+                                                        "Upload",
+                                                        Path.GetRandomFileName());
+                    fileInfo += filePath + Environment.NewLine;
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+            return Ok($"Uploaded: {fileInfo}");
         }
     }
 }
