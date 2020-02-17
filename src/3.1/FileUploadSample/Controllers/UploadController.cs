ec1a7dcd4f5e4967288fcde3f49039d8f015be9d
kkbruce <bruce.mvc@outlook.com>
2020-02-17 21:20:51 +0800

    Add FileUploadSample project

---
+++ src/3.1/FileUploadSample/Controllers/UploadController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadSample.Controllers
+{
+    [Route("Upload/[action]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+    }
+}
