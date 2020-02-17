dae59b014e59ea2e86fa5c02aa5792af844e8054
kkbruce <bruce.mvc@outlook.com>
2020-02-17 21:57:21 +0800

    Add SingleFileSaveDisk sample

--- src/3.1/FileUploadSample/Controllers/UploadController.cs
+++ src/3.1/FileUploadSample/Controllers/UploadController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileUploadSample.Controllers
@@ -7,6 +10,12 @@ namespace FileUploadSample.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public UploadController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
         /// <summary>
         /// Upload a file.
         /// 上傳一個檔案。
@@ -41,5 +50,27 @@ namespace FileUploadSample.Controllers
             return BadRequest("File zero length?");
         }
 
+        /// <summary>
+        /// 上傳一個檔案並儲存至Upload資料夾下。
+        /// </summary>
+        /// <param name="file">upload key name.</param>
+        /// <returns>HTTP 200</returns>
+        [HttpPost]
+        public async Task<IActionResult> SingleFileSaveDisk([FromForm]IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                var filePath = Path.Combine(_hostingEnvironment.ContentRootPath,
+                                                    "Upload",
+                                                    Path.GetRandomFileName());
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                return Ok($"Uploaded: {filePath}");
+            }
+
+            return BadRequest("File zero length?");
+        }
     }
 }
