006736f8048bab5c9973e82dfdf24ea73331656a
kkbruce <bruce.mvc@outlook.com>
2020-02-17 22:24:43 +0800

    remove unuse namespace

--- src/3.1/FileUploadSample/Controllers/UploadController.cs
+++ src/3.1/FileUploadSample/Controllers/UploadController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
 
 namespace FileUploadSample.Controllers
 {
