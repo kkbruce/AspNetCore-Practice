9ff4047d8175cba8f1f74c0b601a41a7b275e069
kkbruce <bruce.mvc@outlook.com>
2020-02-17 21:21:59 +0800

    Add SingleFile sample

--- src/3.1/FileUploadSample/Controllers/UploadController.cs
+++ src/3.1/FileUploadSample/Controllers/UploadController.cs
@@ -7,5 +7,22 @@ namespace FileUploadSample.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
+        /// <summary>
+        /// Upload a file.
+        /// 上傳一個檔案。
+        /// </summary>
+        /// <param name="file">upload key name.</param>
+        /// <returns>HTTP 200</returns>
+        [HttpPost]
+        public IActionResult SingleFile(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                return Ok($"File Length: {file.Length}");
+            }
+
+            return BadRequest("File zero length?");
+        }
+
     }
 }
