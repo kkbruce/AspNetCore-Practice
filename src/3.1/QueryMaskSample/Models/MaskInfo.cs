82310ef78cb49d5f440104bbbf61a62e719d9e40
kkbruce <bruce.mvc@outlook.com>
2020-02-18 21:38:02 +0800

    Add MaskInfo Model

---
+++ src/3.1/QueryMaskSample/Models/MaskInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QueryMaskSample.Models
+{
+    public class MaskInfo
+    {
+        public string 醫事機構代碼 { get; set; }
+        public string 醫事機構名稱 { get; set; }
+        public string 醫事機構地址 { get; set; }
+        public string 醫事機構電話 { get; set; }
+        public string 成人口罩剩餘數 { get; set; }
+        public string 兒童口罩剩餘數 { get; set; }
+        public string 來源資料時間 { get; set; }
+    }
+}
