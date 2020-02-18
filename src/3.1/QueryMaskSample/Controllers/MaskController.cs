b515b857e02d8851b1f79614d9b81702ddf02836
kkbruce <bruce.mvc@outlook.com>
2020-02-18 21:30:40 +0800

    Init QueryMaskSample

---
+++ src/3.1/QueryMaskSample/Controllers/MaskController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QueryMaskSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MaskController : ControllerBase
+    {
+    }
+}
