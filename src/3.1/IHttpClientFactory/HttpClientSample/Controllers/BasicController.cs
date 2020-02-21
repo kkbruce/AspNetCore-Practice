c9725a2f9692904dcdd98a217a79f51caf7e3ce8
kkbruce <bruce.mvc@outlook.com>
2020-02-21 23:37:01 +0800

    Add IHttpClientFactory to ctor

---
+++ src/3.1/IHttpClientFactory/HttpClientSample/Controllers/BasicController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientSample.Controllers
+{
+    [Route("Basic/[action]")]
+    [ApiController]
+    public class BasicController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public BasicController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
