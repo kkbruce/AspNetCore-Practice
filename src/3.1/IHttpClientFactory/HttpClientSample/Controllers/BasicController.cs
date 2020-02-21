a0abb1310e421f4a34949a76330b0e3979a63067
kkbruce <bruce.mvc@outlook.com>
2020-02-21 23:41:59 +0800

    Use HttpClient call out source

--- src/3.1/IHttpClientFactory/HttpClientSample/Controllers/BasicController.cs
+++ src/3.1/IHttpClientFactory/HttpClientSample/Controllers/BasicController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace HttpClientSample.Controllers
 {
@@ -17,6 +14,28 @@ namespace HttpClientSample.Controllers
         public BasicController(IHttpClientFactory httpClient)
         {
             _httpClient = httpClient;
+        }
+
+
+        /// <summary>
+        /// Use Browser test it.
+        /// </summary>
+        /// <returns>UI display content.</returns>
+        public async Task<IActionResult> CallSkilltree()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://skilltree.my");
+            var client = _httpClient.CreateClient();
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return Content(result, new MediaTypeHeaderValue("text/html"));
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
