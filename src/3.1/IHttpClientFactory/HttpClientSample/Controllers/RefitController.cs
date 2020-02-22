251e37d921bff9ab24970ac9c17f2970209ec97b
kkbruce <bruce.mvc@outlook.com>
2020-02-22 11:54:15 +0800

    Add Refit sample

---
+++ src/3.1/IHttpClientFactory/HttpClientSample/Controllers/RefitController.cs
@@ -0,0 +1,20 @@
+﻿using HttpClientSample.Services;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+using System.Threading.Tasks;
+
+namespace HttpClientSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RefitController : ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+            var blogApi = RestService.For<IBlogApi>("https://blog.kkbruce.net");
+            var kkbruce = await blogApi.GetTagName("ASP.NET Core");
+
+            return Ok(kkbruce);
+        }
+    }
+}
---
+++ src/3.1/IHttpClientFactory/HttpClientSample/Services/IBlogApi.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace HttpClientSample.Services
+{
+    public interface IBlogApi
+    {
+            [Get("/search/label/{tagName}")]
+            Task<string> GetTagName(string tagName);
+    }
+}
