ecc63d6efccbef689c878c258de4d89a7434e0e7
kkbruce <bruce.mvc@outlook.com>
2020-02-22 12:01:35 +0800

    Add DelegatingHandler issue sample

---
+++ src/3.1/IHttpClientFactory/HttpClientSample/Handlers/ValidateAccessTokenHeaderHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientSample.Handlers
+{
+    public class ValidateAccessTokenHeaderHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains("X-Access-Token"))
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("You must supply an access token header called X-Access-Token")
+                };
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
