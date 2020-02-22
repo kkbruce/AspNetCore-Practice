0a0216788c941b5067ca8be8300594c8171ebe7c
kkbruce <bruce.mvc@outlook.com>
2020-02-22 14:00:55 +0800

    init httpclientconsolesample

---
+++ src/3.1/IHttpClientFactory/HttpClientConsoleSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HttpClientConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
