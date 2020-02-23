08edada3d7f65eaffee06cc4559cce36bc4eeb36
kkbruce <bruce.mvc@outlook.com>
2020-02-23 11:14:47 +0800

    Call blogService

--- src/3.1/IHttpClientFactory/HttpClientConsoleSample/Program.cs
+++ src/3.1/IHttpClientFactory/HttpClientConsoleSample/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace HttpClientConsoleSample
 {
@@ -17,6 +18,25 @@ namespace HttpClientConsoleSample
                 }).UseConsoleLifetime();
 
             var host = builder.Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+                try
+                {
+                    var blogService = services.GetRequiredService<IBlog>();
+                    var article = await blogService.GetArticle("2020/02/aspnet-core-provider-mask-api.html");
+
+                    // output first 1000 words.
+                    Console.WriteLine(article.Substring(0, 1000));
+                    Console.Read();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred.");
+                }
+            }
         }
     }
 }
