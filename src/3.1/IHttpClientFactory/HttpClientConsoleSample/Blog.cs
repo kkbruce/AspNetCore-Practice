5e9b5cc076a0cf789b7f47b72f36415241149d4a
kkbruce <bruce.mvc@outlook.com>
2020-02-23 11:12:23 +0800

    Add Blog.cs

---
+++ src/3.1/IHttpClientFactory/HttpClientConsoleSample/Blog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientConsoleSample
+{
+    public interface IBlog
+    {
+        Task<string> GetArticle(string article);
+    }
+
+    public class Blog : IBlog
+    {
+        private readonly IHttpClientFactory _client;
+
+        public Blog(IHttpClientFactory client)
+        {
+            _client = client;
+        }
+
+        public async Task<string> GetArticle(string article)
+        {
+            var uri = Path.Combine("https://blog.kkbruce.net/", article);
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            var client = _client.CreateClient();
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return $"StatusCode: {response.StatusCode}";
+            }
+        }
+    }
+}
