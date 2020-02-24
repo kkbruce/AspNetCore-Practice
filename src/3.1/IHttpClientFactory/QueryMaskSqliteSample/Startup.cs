1996e8805589065470f1bce0ec0c353ec63c283d
kkbruce <bruce.mvc@outlook.com>
2020-02-24 21:48:41 +0800

    AddScoped

--- src/3.1/IHttpClientFactory/QueryMaskSqliteSample/Startup.cs
+++ src/3.1/IHttpClientFactory/QueryMaskSqliteSample/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QueryMaskSample.Models;
 using QueryMaskSample.Services;
 
 namespace QueryMaskSample
@@ -19,6 +20,7 @@ namespace QueryMaskSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<MaskContext>();
             services.AddHttpClient<MaskService>();
             services.AddControllers();
         }
