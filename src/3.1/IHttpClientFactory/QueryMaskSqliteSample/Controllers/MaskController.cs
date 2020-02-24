1c8e4bbf3d9ddbe12d570db6282b69c82a47b5c5
kkbruce <bruce.mvc@outlook.com>
2020-02-24 21:41:38 +0800

    Add InitialDB and MaskDataUpdate

--- src/3.1/IHttpClientFactory/QueryMaskSqliteSample/Controllers/MaskController.cs
+++ src/3.1/IHttpClientFactory/QueryMaskSqliteSample/Controllers/MaskController.cs
@@ -1,6 +1,11 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using QueryMaskSample.Models;
 using QueryMaskSample.Services;
 
 namespace QueryMaskSample.Controllers
@@ -9,19 +14,74 @@ namespace QueryMaskSample.Controllers
     [ApiController]
     public class MaskController : ControllerBase
     {
+        private readonly MaskContext _maskContext;
         private readonly MaskService _maskService;
 
-        public MaskController(MaskService maskService)
+        public MaskController(MaskContext maskContext, MaskService maskService)
         {
+            _maskContext = maskContext;
             _maskService = maskService;
         }
 
         public async Task<IActionResult> Get()
         {
+            async Task InitialDB(MedicalMask medical)
+            {
+                if (medical == null)
+                {
+                    var maskInfos = await _maskService.GetMaskInfo();
+                    foreach (var maskInfo in maskInfos)
+                    {
+                        _maskContext.Add(new MedicalMask()
+                        {
+                            Code = maskInfo.醫事機構代碼,
+                            Name = maskInfo.醫事機構名稱,
+                            Address = maskInfo.醫事機構地址,
+                            Tel = maskInfo.醫事機構電話,
+                            AdultNum = int.Parse(maskInfo.成人口罩剩餘數),
+                            KidNum = int.Parse(maskInfo.兒童口罩剩餘數),
+                            UpdateTime = DateTime.Parse(maskInfo.來源資料時間)
+                        });
+                    }
+
+                    await _maskContext.SaveChangesAsync();
+                }
+            }
+
+            async Task MaskDataUpdate()
+            {
+                var nextTime = _maskContext.MedicalMasks.First().UpdateTime.AddSeconds(600);
+                var nowTime = DateTime.Now;
+                if (nextTime < nowTime)
+                {
+                    _maskContext.RemoveRange(_maskContext.MedicalMasks);
+                    var maskInfos = await _maskService.GetMaskInfo();
+                    foreach (var maskInfo in maskInfos)
+                    {
+                        _maskContext.Add(new MedicalMask()
+                        {
+                            Code = maskInfo.醫事機構代碼,
+                            Name = maskInfo.醫事機構名稱,
+                            Address = maskInfo.醫事機構地址,
+                            Tel = maskInfo.醫事機構電話,
+                            AdultNum = int.Parse(maskInfo.成人口罩剩餘數),
+                            KidNum = int.Parse(maskInfo.兒童口罩剩餘數),
+                            UpdateTime = DateTime.Parse(maskInfo.來源資料時間)
+                        });
+                    }
+
+                    await _maskContext.SaveChangesAsync();
+                }
+            }
+
             try
             {
-                var maskCount = await _maskService.GetMaskInfo();
-                return Ok(maskCount);
+                var medical = _maskContext.MedicalMasks.FirstOrDefault();
+                await InitialDB(medical);
+                await MaskDataUpdate();
+
+                return Ok(_maskContext.MedicalMasks.AsNoTracking());
+
             }
             catch (HttpRequestException ex)
             {
