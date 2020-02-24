7f57080c72043bc27aca34ea3d5706098321d7d4
kkbruce <bruce.mvc@outlook.com>
2020-02-24 21:36:34 +0800

    Run migration

---
+++ src/3.1/IHttpClientFactory/QueryMaskSqliteSample/Migrations/20200224133501_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QueryMaskSample.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MedicalMasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Tel = table.Column<string>(nullable: true),
+                    AdultNum = table.Column<int>(nullable: false),
+                    KidNum = table.Column<int>(nullable: false),
+                    UpdateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalMasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MedicalMasks");
+        }
+    }
+}
