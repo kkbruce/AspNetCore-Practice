e6a1858a959872af3fa8190222f5a3cc1d0c54b2
kkbruce <bruce.mvc@outlook.com>
2020-02-24 21:26:49 +0800

    copy from QueryMaskSample

---
+++ src/3.1/IHttpClientFactory/QueryMaskSqliteSample/Controllers/MaskController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QueryMaskSample.Services;
+
+namespace QueryMaskSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MaskController : ControllerBase
+    {
+        private readonly MaskService _maskService;
+
+        public MaskController(MaskService maskService)
+        {
+            _maskService = maskService;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var maskCount = await _maskService.GetMaskInfo();
+                return Ok(maskCount);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
