4c9af9ca3fca4e85e314d591f3cbd5e8b77868d4
kkbruce <bruce.mvc@outlook.com>
2020-02-24 21:31:01 +0800

    Add MedicalMask Model

---
+++ src/3.1/IHttpClientFactory/QueryMaskSqliteSample/Models/MedicalMask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QueryMaskSample.Models
+{
+    public class MedicalMask
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Tel { get; set; }
+        public int AdultNum { get; set; }
+        public int KidNum { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+}
