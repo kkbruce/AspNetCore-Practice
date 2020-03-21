67060d8f9862a9f9fec1b266ea35da5cdfe3ded4
kkbruce <bruce.mvc@outlook.com>
2020-03-21 10:43:03 +0800

    Add Post, Blog models

---
+++ src/3.1/FormatterSample/Models/Blog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FormatterSample.Models
+{
+    public class Blog
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ src/3.1/FormatterSample/Models/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace FormatterSample.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string MetaDescription { get; set; }
+    }
+}
