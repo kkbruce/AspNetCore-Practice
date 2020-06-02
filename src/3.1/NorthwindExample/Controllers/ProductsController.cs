a4294d4ce36c39d191d2ea71702060a4c93d0bd9
kkbruce <bruce.mvc@outlook.com>
2020-06-02 16:29:44 +0800

    add ProductsController

---
+++ src/3.1/NorthwindExample/Controllers/ProductsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NorthwindExample.Models;
+
+namespace NorthwindExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public ProductsController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Products>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProducts(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProducts(int id, Products products)
+        {
+            if (id != products.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(products).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Products>> PostProducts(Products products)
+        {
+            _context.Products.Add(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProducts", new { id = products.ProductId }, products);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Products>> DeleteProducts(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(products);
+            await _context.SaveChangesAsync();
+
+            return products;
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
