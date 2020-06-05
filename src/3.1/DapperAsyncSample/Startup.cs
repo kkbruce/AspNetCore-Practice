294da058259806c5b5681528141e6e8b74adf90f
kkbruce <bruce.mvc@outlook.com>
2020-06-05 09:19:27 +0800

    add Startup

---
+++ src/3.1/DapperAsyncSample/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DapperAsyncSample
+{
+    public static class Startup
+    {
+        public static IConfigurationRoot Configuration(string[] args)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+    }
+}
