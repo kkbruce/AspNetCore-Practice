91756bdb784530b1b0f8970bbe2b21b5f6a9a59b
kkbruce <bruce.mvc@outlook.com>
2020-06-05 09:20:40 +0800

    add Dapper QueryAsync demo

--- src/3.1/DapperAsyncSample/Program.cs
+++ src/3.1/DapperAsyncSample/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 
 namespace DapperAsyncSample
 {
@@ -7,7 +10,29 @@ namespace DapperAsyncSample
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = Startup.Configuration(args);
+            var connString = config.GetConnectionString("NorthwindDatabase");
+            string sqlProducts = "SELECT * FROM Products;";
+            using (var connection = new SqlConnection(connString))
+            {
+                var products = await connection.QueryAsync(sqlProducts).ConfigureAwait(true);
+                Console.WriteLine($"Product counter: {products.AsList().Count}");
+            }
+
+            Console.Read();
+        }
+        internal class Products
+        {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public int? SupplierId { get; set; }
+            public int? CategoryId { get; set; }
+            public string QuantityPerUnit { get; set; }
+            public decimal? UnitPrice { get; set; }
+            public short? UnitsInStock { get; set; }
+            public short? UnitsOnOrder { get; set; }
+            public short? ReorderLevel { get; set; }
+            public bool Discontinued { get; set; }
         }
     }
 }
