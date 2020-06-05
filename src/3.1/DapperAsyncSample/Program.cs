cfafc5a4f2ff291962d35c954c4a2182892924db
kkbruce <bruce.mvc@outlook.com>
2020-06-05 16:26:28 +0800

    重構與加入選單功能

--- src/3.1/DapperAsyncSample/Program.cs
+++ src/3.1/DapperAsyncSample/Program.cs
@@ -10,17 +10,56 @@ namespace DapperAsyncSample
     {
         static async Task Main(string[] args)
         {
-            var config = Startup.Configuration(args);
-            var connString = config.GetConnectionString("NorthwindDatabase");
+              ConsoleKeyInfo cki;
+            // Prevent example from ending if CTL+C is pressed.
+            Console.TreatControlCAsInput = true;
+            Console.WriteLine("Press 0 show Dapper Async demo menu and input key number to run demo.");
+            Console.WriteLine("Press the Escape (Esc) key to quit. \n");
+
+            do
+            {
+                PrintMenu();
+                cki = Console.ReadKey();
+                Console.WriteLine();
+                //Console.Write($"You pressed {cki.KeyChar.ToString()}");
+
+                int value;
+                if (int.TryParse(cki.KeyChar.ToString(), out value))
+                {
+                    var config = Startup.Configuration(args);
+                    var connString = config.GetConnectionString("NorthwindDatabase");
+
+                    switch (value)
+                    {
+                        case 0:
+                            PrintMenu();
+                            break;
+                        case 1:
+                            await DapperQueryAsync(connString);
+                            break;
+                    }
+                }
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dapper  Demo List(\"Esc\" key to quit):");
+            Console.WriteLine("\t1. DapperQueryAsync");
+
+        }
+
+        private static async Task DapperQueryAsync(string connString)
+        {
             string sqlProducts = "SELECT * FROM Products;";
             using (var connection = new SqlConnection(connString))
             {
-                var products = await connection.QueryAsync(sqlProducts).ConfigureAwait(true);
+                var products = await connection.QueryAsync(sqlProducts).ConfigureAwait(false);
                 Console.WriteLine($"Product counter: {products.AsList().Count}");
-            }
-
-            Console.Read();
+            };
         }
+
         internal class Products
         {
             public int ProductId { get; set; }
