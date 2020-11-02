89526b4ef3cc6ff3345ef2baaedaf7efae17d703
kkbruce <bruce.mvc@outlook.com>
2020-11-02 22:31:05 +0800

    Add CustomResult Sample

---
+++ src/3.1/IActionResultSample/Controllers/CustomeResultController.cs
@@ -0,0 +1,61 @@
+﻿using IActionResultSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultSample.Controllers
+{
+    [Route("CustomResult/[action]")]
+    [ApiController]
+    public class CustomResultController : ControllerBase
+    {
+        /// <summary>
+        /// Return ResponseResult, Results include any object data.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<ResponseBase> GetResult200()
+        {
+            var result = new ResponseResult
+            {
+                StatusCodes = StatusCodes.Status200OK.ToString(),
+                Results = new object[]
+                {
+                    new {name = "Sherry", age = 18},
+                    new {name = "Bruce", age = 20}
+                }
+            };
+
+            return result;
+        }
+
+        /// <summary>
+        /// Return ResponseErrorResult, Results include ProblemDetails object.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<ResponseBase> GetResult500()
+        {
+            var result = new ResponseErrorResult
+            {
+                StatusCodes = StatusCodes.Status500InternalServerError.ToString(),
+                Results = new object[]
+                {
+                    new ProblemDetails()
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Type = "https://tools.ietf.org/html/rfc7807",
+                        Title = "BadRequest",
+                        Detail = "One or more client problem.",
+                        Instance = HttpContext.Request.Path,
+                        Extensions =
+                        {
+                            ["tracking-id"] = "9527"
+                        }
+                    }
+                }
+            };
+
+            return result;
+        }
+    }
+}
---
+++ src/3.1/IActionResultSample/Models/ResponseResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IActionResultSample.Models
+{
+    public abstract class ResponseBase
+    {
+        public string StatusCodes { get; set; }
+        public virtual Object[] Results { get; set; }
+    }
+
+    public class ResponseResult : ResponseBase
+    {
+    }
+
+    public class ResponseErrorResult : ResponseBase
+    {
+    }
+}
